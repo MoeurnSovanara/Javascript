649ae94221cb0105315d6854e6610e88941c3b46
MoeurnSovanara <liip28369@gmail.com>
2024-06-11 19:10:26 +0700

    update

---
+++ Loop/forinLoop.js
@@ -0,0 +1,5 @@
+let Array=[20,30,40];
+var data;
+for(i in Array){
+    data += console.log(Array[i]);
+}
---
+++ Loop/forofloop.js
@@ -0,0 +1,5 @@
+let Array = [80,90,100]
+var data;
+for(key of Array){
+    data+=console.log(key);
+}
