73491ec769ad90a3c0101020fb54b5dae2a6876d
MoeurnSovanara <liip28369@gmail.com>
2024-06-13 19:08:04 +0700

    Udate File

--- Method/String.js
+++ Method/String.js
@@ -1,2 +1,9 @@
-let str ="Nara";
+let str ="Nara!";
 console.log(str.charAt(3));
+let str1= "Hello Java";
+console.log(str1.indexOf("va"));
+console.log(str1.toLocaleLowerCase());
+console.log(str1.toLocaleUpperCase());
+var data = str.concat(str1);
+console.log(data);
+console.log(str1.slice(0,5));
